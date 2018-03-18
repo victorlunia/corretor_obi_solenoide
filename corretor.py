b25df81d4dbe9b09b32c5f732ca7705f90c3b4c3
victorlunia <victoraugustoandradesilva@hotmail.com>
2018-03-18 09:52:44 -0400

    agora procura codigos em loop e com erro de compilação

--- corretor.py
+++ corretor.py
@@ -1,7 +1,11 @@
 import csv
 import os
+from subprocess import call
 
 # as pastas "codigos", "entradas" e "saidas", já devem estar criadas.
+
+# tempo de execução máximo
+second_limit = 2
 
 def make_dir():
 	# pasta de saida dos arquivos compilados
@@ -36,16 +40,27 @@ def search_file(file_name):
 
 def compile(file_name, output):
 	"""Funcao que compila o codigo da pessoa"""
-	os.system("g++ " + "codigos/" + file_name + " -o " + "out/" + output)
+	if(call(["g++", "codigos/" + file_name, "-o", "out/" + output]) == 0):
+		return True
+	else:
+		return False
 
 def execute(output, cin, file_output):
 	"""Executa o codigo da pessoa e guarda a saida em um arquivo"""
-	os.system("out/./" + output + " < " + "entradas/" + cin + " > " + "cout/" + file_output)
+	if(os.system("timeout " + str(second_limit) + "s out/./" + output + " < " + "entradas/" + cin + " > " + "cout/" + file_output) == 0):
+		return True
+	else:
+		return False
 
 def correct(output_correct, file_output):
 
 	file_ok = open("saidas/" + output_correct, 'r')
 	file_correct = open("cout/" + file_output, 'r')
+
+	# numero da linha incorreta, linha incorreta
+	incorrect_rows = []
+
+	count_line = 0
 
 	# qtd linhas
 	qtd_lines_file_ok = count_lines(output_correct)
@@ -57,14 +72,20 @@ def correct(output_correct, file_output):
 
 	# compara as saidas
 	for line in file_ok:
+
+		# numero da linha no arquivo
+		count_line += 1
+
 		if(line == line_student):
 			correct += 1
+		else:
+			incorrect_rows.append( (count_line, line_student) )
 
 		line_student = file_correct.readline()
 
-	return (correct/qtd_lines_file_ok)*100
+	return ((correct/qtd_lines_file_ok)*100, incorrect_rows)
 
-def corretor(arq_csv="nomes.csv", arq_entradas, arq_saidas, exercicio):
+def corretor(arq_csv, arq_entradas, arq_saidas, exercicio):
 	"""
 	Responsável por compilar, executar e verificar as respostas
 
@@ -90,15 +111,18 @@ def corretor(arq_csv="nomes.csv", arq_entradas, arq_saidas, exercicio):
 
 			if(search_file(name)):
 				# compila o codigo
-				compile(name, name_output_bin)
-
-				# testa as entradas
-				print("nome:", row[1])
-				for i, cin in enumerate(arq_entradas):
-					# executa
-					execute(name_output_bin, cin, name_output_cout)
-					# verifica corretude
-					print("arquivo", i+1, correct(arq_saida[i], name_output_cout), "% correto")
+				if(compile(name, name_output_bin)):
+					# testa as entradas
+					print("nome:", row[1])
+					for i, cin in enumerate(arq_entradas):
+						# executa
+						if(execute(name_output_bin, cin, name_output_cout)):
+							# verifica corretude
+							print("arquivo", i+1, correct(arq_saida[i], name_output_cout)[0], "% correto")
+						else:
+							print("entrou em loop")
+				else:
+					print("erro de compilação meu chapa!")
 
 arq_csv = "nomes.csv"
 arq_entradas = ["entrada_e1_1.txt","entrada_e1_2.txt"]
