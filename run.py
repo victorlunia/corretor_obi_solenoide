f445013c42399a91a0760067121e1d4cda29dfa4
Victor Augusto <victoraugustoandradesilva@hotmail.com>
2018-03-25 16:29:48 -0400

    os exercicios que serão corrigidos são colocados dentro de um arquivo

--- run.py
+++ run.py
@@ -1,11 +1,15 @@
 import corretor as crt
 import time
-exercicio = ["e1", "e2"]
+import csv
+
+
 arq_csv = "nomes.csv"
 
 while(True):
 	print("executando")
+	exercicio = open("exercicio.csv", 'r')
 	for e in exercicio:
 		crt.corretor(arq_csv, crt.get_arqs(e, "entradas/"), crt.get_arqs(e, "saidas/"), e)
 		time.sleep(5)
+	exercicio.close()
 	time.sleep(60)
